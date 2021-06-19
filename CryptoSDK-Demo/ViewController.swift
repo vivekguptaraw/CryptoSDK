a1322690e83e8eafd45d7740420f8f74f5bf2960
Vivek Gupta <vivek.gupta122@optum.com>
2021-06-19 20:56:18 +0530

    Initial push

---
+++ CryptoSDK-Demo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CryptoSDK-Demo
+//
+//  Created by Gupta, Vivek N on 19/06/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ CryptoSDK/CryptoSDK.h
@@ -0,0 +1,18 @@
+//
+//  CryptoSDK.h
+//  CryptoSDK
+//
+//  Created by Gupta, Vivek N on 19/06/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CryptoSDK.
+FOUNDATION_EXPORT double CryptoSDKVersionNumber;
+
+//! Project version string for CryptoSDK.
+FOUNDATION_EXPORT const unsigned char CryptoSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CryptoSDK/PublicHeader.h>
+
+
